2044c1affb9adfe0532babf5edaa96f7d07975df
Draagn282 <94443911+Draagn282@users.noreply.github.com>
2023-05-11 12:07:05 +0200

    Dit is een commite en hierna ga ik naar branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <>
-      <p className="h-40">help</p>
-    </>
+      <div>
+        <h1>Login</h1>
+        <form action="POST">
+          <label htmlFor="">Username</label>
+          <input type="text" placeholder='Put your name here'/>
+          <label>Password</label>
+          <input type="password" placeholder='passweord here'/>
+        </form>
+      </div>
   )
 }
 
