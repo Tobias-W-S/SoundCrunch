c1850c52e95b98c5e8f8d18d6764cb1c588c55b2
Draagn282 <94443911+Draagn282@users.noreply.github.com>
2023-05-25 15:24:39 +0200

    als je van  pagina naar app gaat brickt de website

--- src/components/chats.jsx
+++ src/components/chats.jsx
@@ -1,11 +1,12 @@
 export const chats = () =>{
     return(
-        <div className="min-w-full min-h-screen backcolor_main">
-            <div className="w-full h-screen flex justify-center items-center">
-                <div className="w-3/4 h-content bg-white rounded-lg flex flex-col">
-                    <div className="w-full h-full">lol</div>
+        <div className="min-w-full min-h-screen h-screen bg-purple-700 flex justify-center items-center">
+
+                <div className="w-3/4 h-content bg-white rounded-lg flex flex-row h-full">
+                    <div className="w-1/3 h-3/4"></div>
+                    <div className="w-1/3 h-3/4"></div>
+                    <div className="w-1/3 h-3/4"></div>
                 </div>
-            </div>
         </div>
     )
 }
