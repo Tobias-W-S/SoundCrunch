a1a3877955a7d462210fcd04b5f92f8da70cd505
Tobias-W-S <1196184@student.roc-nijmegen.nl>
2023-05-25 12:04:29 +0200

    Super basic login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import login from './auth';
+import { logout } from './auth';
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+const user = auth.currentUser
+
 
 function App() {
 
+  const [storedUser, setStoredUser] = useState([]);
+
+  
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+
+      setStoredUser(user);
+      // ...
+    } else {
+      setStoredUser("");
+      // User is signed out
+      // ...
+    }
+  });
+  
+  useEffect(() =>{
+    setStoredUser(user);
+  }, []);
+  
+
   return (
+    
       <div>
-        <h1>Login</h1>
-        <form action="POST">
-          <label htmlFor="">Username</label>
-          <input type="text" placeholder='Put your name here'/>
-          <label>Password</label>
-          <input type="password" placeholder='passweord here'/>
-        </form>
+        <button onClick={login}>Login</button>
+        <br></br>
+        <button onClick={logout}>Logout</button>
+        <div>{storedUser ? storedUser.email : "Not logged in"}</div>
       </div>
   )
 }
