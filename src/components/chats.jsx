a2f45396e1d52b3d343e3017fbee5d49cb1455c1
Draagn282 <94443911+Draagn282@users.noreply.github.com>
2023-05-25 12:05:13 +0200

    oh kak vergetern

---
+++ src/components/chats.jsx
@@ -0,0 +1,13 @@
+export const chats = () =>{
+    return(
+        <div className="min-w-full min-h-screen backcolor_main">
+            <div className="w-full h-screen flex justify-center items-center">
+                <div className="w-3/4 h-content bg-white rounded-lg flex flex-col">
+                    <div className="w-full h-full">lol</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default chats
---
+++ src/components/discover.jsx
@@ -0,0 +1,438 @@
+export const discover = () =>{
+    return(
+        <div className="min-w-full min-h-screen backcolor_main ">
+            <div className="w-full h-screen flex justify-center items-center overflow-hidden">
+                <div className="w-3/4 h-content bg-white rounded-lg flex flex-col">
+                    <div className="w-100 h-100 overflow-auto">[Intro]
+I'm hanging on to the other side
+I won't give up 'til the end of me
+
+[Interlude]
+Go!
+Yeah!
+
+[Refrain]
+Welcome to the mind of a different kind
+Where we've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story
+
+[Pre-Chorus]
+(Face your fear!)
+(Face your fear!)
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gonе
+I'll be the one to carry on (Carry on)
+And you can throw mе to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+You might also like
+Find Your Flame
+Tomoya Ohtani
+Break Through It All
+Tomoya Ohtani
+I’m Here
+Tomoya Ohtani
+[Post-Chorus]
+(Woah, woah)
+You're crossing the line
+(Woah, woah)
+Now we've run out of time
+(Woah, woah)
+I'll take what is mine
+
+[Interlude]
+Hrmm (Go!)
+
+[Bridge]
+And when the story ends
+It becomes a part of me
+I've given what it takes
+I'll find another way
+I used to never know
+But now it's crystal clear!
+This feeling's just a ghost
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gone
+I'll be the one to carry on (Carry on)
+And you can throw me to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+[Guitar Solo]
+
+[Chorus]
+I'm hanging on to the other side (To the other side)
+I won't give up 'til the end of me (End of me)
+I'm what you get when the stars collide (Stars collide)
+Now face it, you're just an enemy
+
+[Refrain]
+Welcome to the mind of a different kind
+We've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story[Intro]
+I'm hanging on to the other side
+I won't give up 'til the end of me
+
+[Interlude]
+Go!
+Yeah!
+
+[Refrain]
+Welcome to the mind of a different kind
+Where we've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story
+
+[Pre-Chorus]
+(Face your fear!)
+(Face your fear!)
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gonе
+I'll be the one to carry on (Carry on)
+And you can throw mе to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+You might also like
+Find Your Flame
+Tomoya Ohtani
+Break Through It All
+Tomoya Ohtani
+I’m Here
+Tomoya Ohtani
+[Post-Chorus]
+(Woah, woah)
+You're crossing the line
+(Woah, woah)
+Now we've run out of time
+(Woah, woah)
+I'll take what is mine
+
+[Interlude]
+Hrmm (Go!)
+
+[Bridge]
+And when the story ends
+It becomes a part of me
+I've given what it takes
+I'll find another way
+I used to never know
+But now it's crystal clear!
+This feeling's just a ghost
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gone
+I'll be the one to carry on (Carry on)
+And you can throw me to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+[Guitar Solo]
+
+[Chorus]
+I'm hanging on to the other side (To the other side)
+I won't give up 'til the end of me (End of me)
+I'm what you get when the stars collide (Stars collide)
+Now face it, you're just an enemy
+
+[Refrain]
+Welcome to the mind of a different kind
+We've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story[Intro]
+I'm hanging on to the other side
+I won't give up 'til the end of me
+
+[Interlude]
+Go!
+Yeah!
+
+[Refrain]
+Welcome to the mind of a different kind
+Where we've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story
+
+[Pre-Chorus]
+(Face your fear!)
+(Face your fear!)
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gonе
+I'll be the one to carry on (Carry on)
+And you can throw mе to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+You might also like
+Find Your Flame
+Tomoya Ohtani
+Break Through It All
+Tomoya Ohtani
+I’m Here
+Tomoya Ohtani
+[Post-Chorus]
+(Woah, woah)
+You're crossing the line
+(Woah, woah)
+Now we've run out of time
+(Woah, woah)
+I'll take what is mine
+
+[Interlude]
+Hrmm (Go!)
+
+[Bridge]
+And when the story ends
+It becomes a part of me
+I've given what it takes
+I'll find another way
+I used to never know
+But now it's crystal clear!
+This feeling's just a ghost
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gone
+I'll be the one to carry on (Carry on)
+And you can throw me to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+[Guitar Solo]
+
+[Chorus]
+I'm hanging on to the other side (To the other side)
+I won't give up 'til the end of me (End of me)
+I'm what you get when the stars collide (Stars collide)
+Now face it, you're just an enemy
+
+[Refrain]
+Welcome to the mind of a different kind
+We've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story[Intro]
+I'm hanging on to the other side
+I won't give up 'til the end of me
+
+[Interlude]
+Go!
+Yeah!
+
+[Refrain]
+Welcome to the mind of a different kind
+Where we've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story
+
+[Pre-Chorus]
+(Face your fear!)
+(Face your fear!)
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gonе
+I'll be the one to carry on (Carry on)
+And you can throw mе to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+You might also like
+Find Your Flame
+Tomoya Ohtani
+Break Through It All
+Tomoya Ohtani
+I’m Here
+Tomoya Ohtani
+[Post-Chorus]
+(Woah, woah)
+You're crossing the line
+(Woah, woah)
+Now we've run out of time
+(Woah, woah)
+I'll take what is mine
+
+[Interlude]
+Hrmm (Go!)
+
+[Bridge]
+And when the story ends
+It becomes a part of me
+I've given what it takes
+I'll find another way
+I used to never know
+But now it's crystal clear!
+This feeling's just a ghost
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gone
+I'll be the one to carry on (Carry on)
+And you can throw me to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+[Guitar Solo]
+
+[Chorus]
+I'm hanging on to the other side (To the other side)
+I won't give up 'til the end of me (End of me)
+I'm what you get when the stars collide (Stars collide)
+Now face it, you're just an enemy
+
+[Refrain]
+Welcome to the mind of a different kind
+We've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story[Intro]
+I'm hanging on to the other side
+I won't give up 'til the end of me
+
+[Interlude]
+Go!
+Yeah!
+
+[Refrain]
+Welcome to the mind of a different kind
+Where we've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story
+
+[Pre-Chorus]
+(Face your fear!)
+(Face your fear!)
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gonе
+I'll be the one to carry on (Carry on)
+And you can throw mе to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+You might also like
+Find Your Flame
+Tomoya Ohtani
+Break Through It All
+Tomoya Ohtani
+I’m Here
+Tomoya Ohtani
+[Post-Chorus]
+(Woah, woah)
+You're crossing the line
+(Woah, woah)
+Now we've run out of time
+(Woah, woah)
+I'll take what is mine
+
+[Interlude]
+Hrmm (Go!)
+
+[Bridge]
+And when the story ends
+It becomes a part of me
+I've given what it takes
+I'll find another way
+I used to never know
+But now it's crystal clear!
+This feeling's just a ghost
+
+[Chorus]
+It's time to face your fear
+'Cause when your time has come and gone
+I'll be the one to carry on (Carry on)
+And you can throw me to the wolves (Throw me to the wolves)
+'Cause I am undefeatable
+I'm hanging on to the other side
+I won't give up 'til the end of me
+I'm what you get when the stars collide
+Now face it, you're just an enemy
+[Guitar Solo]
+
+[Chorus]
+I'm hanging on to the other side (To the other side)
+I won't give up 'til the end of me (End of me)
+I'm what you get when the stars collide (Stars collide)
+Now face it, you're just an enemy
+
+[Refrain]
+Welcome to the mind of a different kind
+We've been growing slowly
+Think I'm on eleven, but I'm on a nine
+Guess you don't really know me
+Running from the past is a losing game
+It never brings you glory
+Been down this road before
+Already know this story</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default discover
