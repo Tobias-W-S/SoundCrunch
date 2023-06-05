e4e53648c07e23cefe497f1020b2b32df361462c
Tobias-W-S <1196184@student.roc-nijmegen.nl>
2023-06-05 10:56:26 +0200

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,9 @@
-
-import { useState, useEffect } from 'react';
-import login from './auth';
-import { logout } from './auth';
-
-
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-const auth = getAuth();
-const user = auth.currentUser
-
-
-
 function App() {
 
-  const [storedUser, setStoredUser] = useState([]);
-
-  
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-
-      setStoredUser(user);
-      // ...
-    } else {
-      setStoredUser("");
-      // User is signed out
-      // ...
-    }
-  });
-  
-  useEffect(() =>{
-    setStoredUser(user);
-  }, []);
-  
 
   return (    
       <div>
-        <button onClick={login}>Login</button>
-        <br></br>
-        <button onClick={logout}>Logout</button>
-        <div>{storedUser ? storedUser.email : "Not logged in"}</div>
+        test
       </div>
   )
 }
