196039f4b747b8d911f543c07adb914a536a8feb
Draagn282 <94443911+Draagn282@users.noreply.github.com>
2023-06-08 11:49:21 +0200

    push de chat en begin profile

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,21 +2,32 @@ export const home = () =>{
     return(
         <div className="min-w-full min-h-screen bg-purple-700 flex justify-center items-center">
                 <div className="h-10"></div>
-                <div className="w-3/4 h-content bg-white rounded-lg flex flex-col">
-                    <div className="flex flex-row h-56 w-full">
-                        <img src="" alt="" />
-                        <label htmlFor="">...NAME...</label>
-                        <br/><label htmlFor="">...EMAIL...</label>
-                        <br/><label htmlFor="">...Total songs...</label>
-                        <br/><label htmlFor="">...Total friends...</label>
-                        <br/><label htmlFor="">...Total time a user i guess, tobias als je dit leest verin wat...</label>
+                <div className="w-3/4 h-content bg-white rounded-lg flex flex-col ">
+                    <div className="flex flex-row h-96 w-full m-4">
+                        <div className="w-96 h-full bg-green-700 flex items-center justify-center">
+                            <div className="w-3/4 h-3/4 bg-red-200">
+                            <img src="" alt="Profile pick"/>
+                            </div>
+                        </div>
+                        <div className="h-full w-2/3 bg-red-300">
+                            <ul className="h-3/4 flex flex-col justify-around">
+                                <li className="font-semibold text-xl border">...NAME...</li>
+                                <li>...EMAIL...</li>
+                                <li>...LISTENS...</li>
+                                <li>...SONGS...</li>
+                                <li>...LIKES...</li>
+                            </ul>
+                        </div>
+                        <div className="h-full w-1/4 bg-blue-300 flex flex-col justify-start">
+                            <p className="font-semibold text-xl">Top SONGS:</p>
+                            <p>MP3 sounds</p>
+                            <p>MP3 sounds</p>
+                            <p>MP3 sounds</p>
+                        </div>
                     </div>
-                    <div className="flex flex-row h-56 w-full">
+                    <div className="flex flex-row h-96 w-full">
                         <a href=""><button>...Create friend request...</button></a>
                         <a href=""><button>...Message...</button></a> 
-                    </div>
-                    <div className="flex flex-row h-56 w-full">
-                        
                     </div>
                 </div>
         </div>
